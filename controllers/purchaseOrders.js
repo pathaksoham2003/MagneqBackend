5dd2ad30d14b3a37a4b3d3544fe0d7f31455e146
pathaksoham2003 <pathaksoham2003@gmail.com>
2025-07-29 01:04:18 +0530

    Got the all purchases

--- controllers/purchaseOrders.js
+++ controllers/purchaseOrders.js
@@ -52,7 +52,7 @@ export const getAllPurchases = async (req, res) => {
   try {
     const pageNo = parseInt(req.query.page_no) || 1;
     const PAGE_SIZE = 10;
-    const filter = { status: { $ne: "COMPLETE" } };
+    const filter = {  };
     const totalCount = await Purchase.countDocuments(filter);
 
     const purchases = await Purchase.find(filter)
