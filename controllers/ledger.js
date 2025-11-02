b0dff361675fa8c4f89294486f46b91a83bbab15
pathaksoham2003 <pathaksoham2003@gmail.com>
2025-11-02 13:52:22 +0530

    Implemented date include

--- controllers/ledger.js
+++ controllers/ledger.js
@@ -10,7 +10,9 @@ import { getLastRunningBalance } from "../utils/ledgerUtils.js";
  */
 export const getLedgerData = async (customerId, startDate, endDate) => {
   const start = new Date(startDate);
+  start.setHours(0, 0, 0, 0);
   const end = new Date(endDate);
+  end.setHours(23, 59, 59, 999);
 
   // 1️⃣ Get opening balance before start date
   const previousEntry = await Ledger.findOne({
